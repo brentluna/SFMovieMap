83b30f00c8edd925153274a178887bc7506755d3
brentluna <brentadamluna@gmail.com>
2016-10-07 18:26:40 -0700

    added intro modal to explainn app

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,17 +4,55 @@ import MapContainer from './map/map_container';
 import SearchContainer from './search_container';
 import Navbar from './navbar/navbar';
 import SearchResultsContainer from './search/search_results_container';
+import Modal from 'react-modal';
+
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    maxWidth              : '400px',
+    background            : 'whitesmoke'
+  }
+};
+
 
 class Root extends React.Component {
   constructor(props) {
     super(props);
+    this.closeModal = this.closeModal.bind(this);
+    this.state = {modalIsOpen: true};
   }
+
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+
 
   render() {
 
     return(
       <Provider store={this.props.store}>
+
       <div>
+      <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles} >
+
+          <h1>SF Film App</h1>
+          <div className='modal-text'>Search for movies and shows that have been filmed in San Francisco. You can search by title, actor, directors, and locations. Click on map markers to get more details, or click on 'Plot' or 'Locations' in the search results.</div>
+          <button onClick={this.closeModal}>close</button>
+          
+        </Modal>
+
 
         <Navbar />
         <div className='map-search'>
--- frontend/movie_map.jsx
+++ frontend/movie_map.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import Modal from 'react-modal';
 
 document.addEventListener('DOMContentLoaded', () => {
+  Modal.setAppElement(document.body);
+
   const store = configureStore();
   const rootEl = document.getElementById('rootIndex');
   window.store = store;
